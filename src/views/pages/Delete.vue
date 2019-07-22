94c760d179a0b92f9fc35e52325968ba06536ca6
Boidurja Talukdar <bikloo.talukdar@gmail.com>
2019-07-22 17:35:23 +0530

    commented

--- src/views/pages/Delete.vue
+++ src/views/pages/Delete.vue
@@ -1,8 +1,17 @@
 <template>
     <v-container>
         <div class="mt-3">
-            <v-text-field v-model="idToDelete" type="number" hide-details outline label="Enter Id to delete"></v-text-field>
-            <v-btn @click="userIdtoDelete()" color="error">Delete</v-btn>
+            <form>
+            <v-text-field 
+                v-model="idToDelete" 
+                type="number" 
+                hide-details 
+                outline 
+                label="Enter Id to delete"
+            >
+            </v-text-field>
+            <v-btn @click="userIdtoDelete" color="error">Delete</v-btn>
+            </form>
         </div>
     </v-container>
 </template>
@@ -23,16 +32,22 @@ export default {
             idToDelete: '',
             ids: [],
             i: ''
+            
         }
     },
     methods: {
         userIdtoDelete () {
 
+            /*if (this.idToDelete == '') {
+                alert('Empty Field')
+                return false
+            }*/
+
+             //Vue.axios.delete('http://localhost:3000/users/' + this.idToDelete)
+
             /*axios.get('http://localhost:3000/users')
                 .then(response => { 
-                    this.users2 = response.data 
-
-                    //this.index = this.users.indexOf(this.idToDelete)
+                    this.users2 = response.data
 
                     for (this.i = 0; this.i < this.users2.length; this.i++) {
                         this.ids[this.i] = this.users2[this.i].id
@@ -43,26 +58,39 @@ export default {
                 })
                 .catch(function (error) {
                     console.log(error);
-            })
+            })*/
 
-            axios.delete('http://localhost:3000/users/' + this.idToDelete)
+           
+
+            /*axios.delete('http://localhost:3000/users/' + this.idToDelete)
             .then(response => {
                 this.users2.splice(this.index, 1)
             })*/
+
             //alert('http://localhost:3000/users/' + this.idToDelete)
-            //Vue.axios.delete('http://localhost:3000/users/' + this.idToDelete )
             
-            axios.delete('http://localhost:3000/users/', {params: {id:  this.idToDelete} })
+            //Vue.axios.delete('http://localhost:3000/users/' + id)
+
+            //alert($event.target.value)
+
+
+            /*Vue.axios.delete(`http://localhost:3000/users/${encodeURIComponent(this.idToDelete)}`)
+            .then(res => { console.log(res) })
+            .catch(err => { console.error(err) })*/
+            
+
+            /*axios.delete('http://localhost:3000/users', {params: {id:  this.idToDelete} })
             .then((response) => {
                 console.log(response)
                 //alert('response = ' + response)
             }, (error) => {
                 console.log(error)
                 //alert('error = ' + error)
-            })
+            })*/
 
             //alert('Id to delete = ' + this.idToDelete)
             //Vue.axios.delete('http://localhost:3000/users', {params: {id:  this.idToDelete} })
+
         }
     }
 }
--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -6,22 +6,7 @@
               <v-toolbar dark color="primary">
                 <v-toolbar-title>Login form</v-toolbar-title>
                 <v-spacer></v-spacer>
-                <!--v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                      <v-icon large>code</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank" v-on="on">
-                      <v-icon large>mdi-codepen</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Codepen</span>
-                </v-tooltip-->
+                
               </v-toolbar>
               <v-card-text>
                 <v-form>
@@ -31,7 +16,7 @@
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary">Login</v-btn>
+                <v-btn @click="login" color="primary">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -47,6 +32,11 @@
     }),
     props: {
       source: String
+    },
+    methods: {
+      login () {
+        this.$router.push({ name: 'get' })
+      }
     }
   }
 </script>
